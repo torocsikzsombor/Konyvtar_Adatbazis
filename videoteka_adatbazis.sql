c483fadef16b4d35087fd908532a9cefa2c387f9
torocsikzsombor <torocsik.zsombor@diak.szbi-pg.hu>
2025-11-27 13:48:08 +0100

    videoteka félkész

---
+++ videoteka_adatbazis.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS videotekaadatbazis;
+CREATE DATABASE videotekaadatbazis
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_hungarian_ci;
+
+
+USE videotekaadatbazis;
+
+
+CREATE TABLE filmek (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cim VARCHAR(150) NOT NULL UNIQUE,
+    rendezo VARCHAR(120),
+    megjelenesi_ev YEAR,
+    hossz_perc INT CHECK (hossz_perc BETWEEN 30 AND 400),
+    mufaj ENUM('akcio', 'vigjatek', 'drama', 'thriller'),
+    imdb_ertekeles FLOAT DEFAULT 6.5
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+
+
+CREATE TABLE kolcsonzok (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nev VARCHAR(100) NOT NULL,
+    email VARCHAR(120) NOT NULL UNIQUE,
+    varos VARCHAR(50) DEFAULT 'Budapest',
+    aktiv ENUM('igen','nem') DEFAULT 'igen',
+    regisztracio DATETIME DEFAULT CURRENT_TIMESTAMP
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+
+
+DESCRIBE filmek;
+
+
+SHOW CREATE TABLE kolcsonzok;
